b0d51fbe79c1e646b2dcab6c153270b817cb1202
Joana Amorim <138633905+joanasouzaa@users.noreply.github.com>
2025-06-09 11:35:41 -0300

    Update PaginationComponent.vue

--- src/components/PaginationComponent.vue
+++ src/components/PaginationComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-center gap-6 mt-8">
-    <button @click="$emit('anterior')" class="btn px-4 py-2 rounded-lg bg-transparent text-black shadow hover:bg-[#89b789] hover:text-white transition">Anterior</button>
-    <button @click="$emit('proximo')" class="btn px-4 py-2 rounded-lg bg-transparent text-black shadow hover:bg-[#89b789] hover:text-white transition">Próximo</button>
+    <button @click="$emit('anterior')" class="p-2 rounded-md bg-transparent text-black shadow hover:bg-[#89b789] hover:text-white transition">Anterior</button>
+    <button @click="$emit('proximo')" class="p-2 rounded-md bg-transparent text-black shadow hover:bg-[#89b789] hover:text-white transition">Próximo</button>
   </div>
 </template>
